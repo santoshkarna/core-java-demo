9495904cbec759cebefe97a8f8b4a16cf6d34358
Pragyan <pragyang@gmail.com>
2025-08-19 19:56:41 -0400

    added git demo file

--- src/day5/gitdemo/GitDemo.java
+++ src/day5/gitdemo/GitDemo.java
@@ -2,6 +2,6 @@ package day5.gitdemo;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello, I am learning git.");
+        System.out.println("Hello I'm learning git ");
     }
 }
